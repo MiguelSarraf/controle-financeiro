6c638f2c089d4037d6c01e816f70b22d0d681d2b
Miguel Sarraf <miguel.sarraf@alumni.usp.br>
2026-09-12 15:34:28 -0300

    HOTFIX: teste issue #5

--- dados.py
+++ dados.py
@@ -256,6 +256,9 @@ def agrega_despesa_parceladas(despesa, fatura):
 def agrega_rendimentos_por_mes(receita, fatura):
     datas = gera_datas_do_ano(fatura)
 
+    print(receita)
+    print(receita[receita["aplicacao"]!="nan"].merge(datas, on="data"))
+
     return receita[receita["aplicacao"]!="nan"].merge(datas, on="data")
 
 def agrega_rendimentos_por_mes_pctg(aplicacoes, receita, fatura):
